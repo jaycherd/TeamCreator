27ab73653c01371add558f452839fab0bf005726
jaycherd <jaycherd@gmail.com>
2023-08-19 11:24:42 -0700

    contains variables that user must change to use

---
+++ config.py
@@ -0,0 +1,15 @@
+################################################################################################################################
+#ALTER the values within the comment block
+# Specify the team size
+team_size = 3
+number_of_teams = 3
+# min num days youd like the team members to have in common
+minDaysOverlap = 1
+# the min num hours of overlap
+minHoursOverlap = 1
+# the name of the csv file
+csv_availability_filename = 'availability.csv'
+csv_priority_filename = 'groupPriority.csv'
+# # to see some extra info for debugging purposes make this var True
+debug = False
+################################################################################################################################
