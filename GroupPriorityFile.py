af1c2e876373e5131a7ea7ef93b6d91a4710caf6
jaycherd <jaycherd@gmail.com>
2023-08-19 13:56:09 -0700

    altered to work with comboHolder

--- GroupPriorityFile.py
+++ GroupPriorityFile.py
@@ -13,8 +13,22 @@ class GroupPriorityFile:
     def __init__(self,filename) -> None:
         self.filename = filename
         df = pd.read_csv(self.filename,header=None)
-        self.group1 = df.loc[0,1:].values.flatten().tolist()
-        self.group2 = df.loc[1,1:].values.flatten().tolist()
-        self.group3 = df.loc[2,1:].values.flatten().tolist()
+        tmp1 = df.loc[0,1:].values.flatten().tolist()
+        tmp2 = df.loc[1,1:].values.flatten().tolist()
+        tmp3 = df.loc[2,1:].values.flatten().tolist()
+        for member in tmp1:
+            if pd.isna(member):
+                continue
+            self.group1.append(member)
+        for member in tmp2:
+            if pd.isna(member):
+                continue
+            self.group2.append(member)
+        for member in tmp3:
+            if pd.isna(member):
+                continue
+            self.group3.append(member)
+        # printGroups(self.group1,self.group2,self.group3)
+        
 
 
